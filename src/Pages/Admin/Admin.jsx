703c51c81de64138d443d91c3b34dbce0fb03886
Naman <namankumar62039@gmail.com>
2024-04-30 01:18:32 +0530

    creating new folders for admin panel

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import "./Admin.css";
 
 const Admin = () => {
-  return (
-    <div>Admin</div>
-  )
-}
+  return <div className="admin">
+    Admin
+    </div>;
+};
 
-export default Admin
+export default Admin;
