ba83e3b88cf706e1d65d002fde35d13efc6bd95c
Naman <namankumar62039@gmail.com>
2024-05-23 02:01:31 +0530

    formateed the code

--- src/Components/ListProduct/ListProduct.jsx
+++ src/Components/ListProduct/ListProduct.jsx
@@ -1,35 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import './ListProduct.css';
-import cross_icon from '../../assets/cross_icon.png';
+import React, { useEffect, useState } from "react";
+import "./ListProduct.css";
+import cross_icon from "../../assets/cross_icon.png";
 
 const ListProduct = () => {
-
   const [allProducts, setallProducts] = useState([]);
 
   const fetchInfo = async () => {
-    await fetch('https://nubifashon-backend.onrender.com/api/v1/upload/allProducts')
-    .then((resp) => resp.json()).then((data) =>  { setallProducts(data?.data)})
-    
-  }
+    await fetch(
+      "https://nubifashon-backend.onrender.com/api/v1/upload/allProducts"
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        setallProducts(data?.data);
+      });
+  };
 
   useEffect(() => {
     fetchInfo();
-  },[])
+  }, []);
 
   const removeProduct = async (id) => {
-    await fetch('https://nubifashon-backend.onrender.com/api/v1/upload/removeProduct', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({id:id})
-    })
+    await fetch(
+      "https://nubifashon-backend.onrender.com/api/v1/upload/removeProduct",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ id: id }),
+      }
+    );
     await fetchInfo();
-  }
+  };
 
   return (
-    <div className='listProduct'>
+    <div className="listProduct">
       <h1>All Product List</h1>
       <div className="listproduct-formate-main">
         <p>Product</p>
@@ -42,21 +48,32 @@ const ListProduct = () => {
       <div className="listProduct-allProducts">
         <hr />
         {allProducts.map((product, index) => {
-          return <>
-          <div key={index} className='listproduct-formate-main listproduct-formate'>
-            <img src={product.images} alt="ProductIcon" className='listProduct-icon' />
-            <p>{product.name}</p>
-            <p>${product.oldPrice}</p>
-            <p>${product.newPrice}</p>
-            <p>{product.category}</p>
-            <img src={cross_icon} onClick={() => removeProduct(product.id)} alt="removeIcon" className='listProduct-removeIcon' />
-          </div>
-          <hr />
-          </>
+          return (
+            <div key={index}>
+              <div className="listproduct-formate-main listproduct-formate">
+                <img
+                  src={product.images}
+                  alt="ProductIcon"
+                  className="listProduct-icon"
+                />
+                <p>{product.name}</p>
+                <p>${product.oldPrice}</p>
+                <p>${product.newPrice}</p>
+                <p>{product.category}</p>
+                <img
+                  src={cross_icon}
+                  onClick={() => removeProduct(product.id)}
+                  alt="removeIcon"
+                  className="listProduct-removeIcon"
+                />
+              </div>
+              <hr />
+            </div>
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListProduct
+export default ListProduct;
