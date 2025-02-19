53b3450e9d93152ee9c363813b2c442c9e21f5a3
souamya <namankumar62039@gmail.com>
2025-02-20 01:15:49 +0530

    creating the all order show page and made it responsive

--- src/Components/AllOrders/AllOrders.jsx
+++ src/Components/AllOrders/AllOrders.jsx
@@ -1,21 +1,23 @@
 import "./AllOrders.css";
 import React, { useEffect, useState } from "react";
+import cross_icon from "../../assets/cross_icon.png";
 
 const AllOrders = () => {
   const [allOrders, setallOrders] = useState([]);
 
-    useEffect(() => {
-      fetch("https://nubifashon-backend.onrender.com/api/v1/order/allOrders")
-        .then((resp) => resp.json())
-        .then((data) => {
-          setallOrders(data?.data.map((e) => e.orderItems))
-        });
-    }, []);
+  useEffect(() => {
+    fetch("https://nubifashon-backend.onrender.com/api/v1/order/allOrders")
+      .then((resp) => resp.json())
+      .then((data) => data?.data.map((e) => e.orderItems))
+      .then((e) => e.flat())
+      .then((e) => setallOrders(e));
+  }, []);
 
-    // console.log(...allOrders);
+  console.log(allOrders);
 
   return (
     <div className="CartItems">
+      <h1>All Orders By Your Customers</h1>
       {allOrders ? (
         <div className="cartItems-format-main">
           <p>Products</p>
@@ -23,19 +25,17 @@ const AllOrders = () => {
           <p>Price</p>
           <p>Size</p>
           <p>Quantity</p>
-          <p>Total</p>
           <p>Remove</p>
         </div>
       ) : (
         <h1>No Orders Right Now!</h1>
       )}
-
+<div className="main">
       <hr />
       {allOrders?.map((e, index) => {
-        console.log(e)
-        if (true) {
+        if (allOrders) {
           return (
-            <div key={index}>
+            <div key={index}> 
               <div className="cartItems-Formate cartItems-format-main">
                 <img
                   src={e.images}
@@ -44,16 +44,13 @@ const AllOrders = () => {
                 />
                 <p>{e.name}</p>
                 <p>₹{e.newPrice}</p>
-                {/* <p>{cartItems[e.id].productSize}</p> */}
+                <p>{e.size}</p>
                 <div className="quantitySection">
-                  <button className="cartItems-quantity">
-                    {/* {cartItems[e.id].quantity} */}
-                  </button>
+                  <button className="cartItems-quantity">{e.quantity}</button>
                 </div>
-                {/* <p>₹{e.newPrice * cartItems[e.id].quantity}</p> */}
                 <img
                   className="cartItems-remove-icon"
-                  // src={remove_icon}
+                  src={cross_icon}
                   onClick={() => {
                     // removefromCart(e.id);
                   }}
@@ -64,8 +61,8 @@ const AllOrders = () => {
             </div>
           );
         }
-        return null;
       })}
+      </div>
     </div>
   );
 };
